c81e352953c7daf8f6aaa9ab5375ba70974cef5b
Ironspider42 <deepeshgurnani7@gmail.com>
2023-09-17 20:16:13 +0530

    Updated coming soon jpg

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import ComingSoon from '../assets/coming-soon.jpg'
+import ComingSoon from '../assets/Coming Soon.gif'
 function Services() {
   return (
-    <div className='w-[1200px]'>
-        <img src={ComingSoon} alt="Coming Soon" className=" flex w-[1275px] min-h-screen justify-center align-middle rounded-lg shadow-lg text-white" />
+    <div className='w-[1200px] mb-10 flex justify-center items-center'>
+        <img src={ComingSoon} alt="Coming Soon" className=" flex top-0 bottom-0 w-[1080px] h-auto rounded-lg shadow-lg" />
     </div>
   )
 }
